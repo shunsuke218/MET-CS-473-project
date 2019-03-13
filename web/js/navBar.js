bcb66abad6e4d1f9cf065c8b68f83b7d1a4e364f
Naima Matahri <nmatahri@bu.edu>
2019-03-13 19:30:01 -0400

    working on home page

---
+++ web/js/navBar.js
@@ -0,0 +1,26 @@
+console.clear();
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLiens = document.querySelectorAll('.nav-links li');
+    //Toggle nav 
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        //Animate links
+        navLiens.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                console.log(index / 5);
+            }
+        });
+        //burger animation
+
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+
+navSlide();
