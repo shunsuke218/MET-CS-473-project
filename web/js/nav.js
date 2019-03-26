0a794a642228c3dfefd2697584e79680e3126e1f
Steve Mu <qmu@bu.edu>
2019-03-26 14:07:35 -0400

    login works again

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,28 +1,7 @@
-//         responseType: 'token',
-
-// var options = {
-//     auth: {
-
-
-//         params: { scope: 'openid email profile', }
-//     }
-// };
-
-// var lock = new Auth0Lock(
-//     'vFJIRuqMjrla9QBtHjvLGFeWz4gENqZi',
-//     'cs473familytree.auth0.com',
-//     // options
-// );
-
-
-// lock.on("authenticated", async function (authResult) {
-//     console.log(authResult);
-//     // debugger;
-//     // await localLoginSuccessSimple(lock, authResult);
-//     // displayButtons();
-// });
+"use strict";
 
 window.addEventListener('load', function () {
+
     var webAuth = new auth0.WebAuth({
         domain: 'cs473familytree.auth0.com',
         clientID: 'vFJIRuqMjrla9QBtHjvLGFeWz4gENqZi',
@@ -31,42 +10,27 @@ window.addEventListener('load', function () {
         redirectUri: window.location.href,
         audience: 'http://localhost:5005/api',
     });
-    
 
-    function onLogin() {
+    // will be successful after redirected from auth0
+    function checkShouldHandleLogin() {
         // on login
-        console.log('will parse harh');
         webAuth.parseHash(function (err, authResult) {
-            if (err) {
+            if (err || !authResult) {
                 // error
-                console.log('error login');
-                console.log(err);
+                // console.log('parseHash from url not successful');
+                // console.log(err);
                 return
             }
-
-            console.log('login');
-            console.log(authResult);
+      
             onLoginSuccess(authResult)
         })
 
     }
 
+    checkShouldHandleLogin();
 
-    // called on refresh
-    if (localStorage.getItem('isLoggedIn') === 'true') {
-        renewTokens(webAuth, (err) => {
-            //error
-            console.log('error renew token');
-            console.log(err);
-        }, () => {
-            // success
-            console.log('renew token success');
-            //todo: refresh button
-        });
-    } else {
-        // called when redirected back from auth0
-        onLogin();
-    }
+    // todo: renew token if already logged
+    // todo: renew token peridlally even without refresh
 
     // login button
     var loginBtn = document.getElementById('btn-login');
@@ -87,7 +51,7 @@ window.addEventListener('load', function () {
     // check should display login or logout button
     function displayButtons() {
         // console.log(isAuthenticated());
-        if (isAuthenticatedSimple()) { // todo
+        if (isAuthenticated()) { // todo
             loginBtn.style.display = 'none';
             logoutBtn.style.display = 'block';
         } else {
--- web/js/utils/utils.js
+++ web/js/utils/utils.js
@@ -1,32 +1,4 @@
-
-
-
-/*
-      give this: http://localhost:5005/#/family-tree
-      return this: http://localhost:5005/
-      */
-function getSiteRootFromUrl(url) {
-    var parser = document.createElement('a');
-    parser.href = url;
-    let siteRoot = parser.protocol + "//" + parser.host;
-    return siteRoot;
-}
-
-// cannot do this
-// var lock = new Auth0Lock(
-//     'vFJIRuqMjrla9QBtHjvLGFeWz4gENqZi',
-//     'cs473familytree.auth0.com'
-// );
-
-// function getAuth0Lock() {
-
-//     var lock = new Auth0Lock(
-//         'vFJIRuqMjrla9QBtHjvLGFeWz4gENqZi',
-//         'cs473familytree.auth0.com'
-//     );
-
-//     return lock;
-// }
+"use strict";
 
 function isAuthenticated() {
     // Check whether the current time is past the
@@ -35,7 +7,7 @@ function isAuthenticated() {
     // console.log(expiresAt);
     var expiration = parseInt(expiresAt) || 0;
     // console.log(expiresAt);
-    return localStorage.getItem('isLoggedIn') === 'true' && new Date().getTime() < expiration;
+    return localStorage.getItem('accessToken') && new Date().getTime() < expiration;
 }
 
 function onLoginSuccess(authResult, cb = () => { }) {
@@ -52,18 +24,6 @@ async function localLoginSuccessSimple(lock, authResult) {
     localStorage.setItem("profile", JSON.stringify(profile));
     return profile;
 }
-
-// function getUserInfoFromAuth0(lock, accessToken) {
-//     return new Promise((resolve, reject) => {
-//         lock.getUserInfo(accessToken, function (error, profile) {
-//             if (!error) {
-//                 resolve(profile);
-//             } else {
-//                 reject();
-//             }
-//         });
-//     })
-// }
 
 function isAuthenticatedSimple() {
     let token = localStorage.getItem('accessToken');
@@ -92,30 +52,6 @@ function renewTokens(webAuth, onError, onSuccess) {
     });
 }
 
-// function getUserEmailWithAccessToken(lock, token) {
-//     return new Promise((resolve, reject) => {
-//         // Use the token in authResult to getUserInfo() and save it to localStorage
-//         lock.getUserInfo(token, function (error, profile) {
-//             if (error) { reject(); }
-//             // store user's identify
-//             let email = profile.email;
-//             resolve(email);
-//         });
-//     })
-// }
-
-// async function getUserEmail() {
-//     let lock = await getAuth0Lock();
-//     let token = localStorage.getItem("accessToken");
-//     let email = await getUserEmailWithAccessToken(lock, token);
-//     return email;
-// }
-
-// function getUserProfile() {
-//     let profileStr = localStorage.getItem("profile");
-//     let profileObj = JSON.parse(profileStr);
-//     return profileObj;
-// }
 
 function auth0Logout(cb) {
     localStorage.removeItem('isLoggedIn');
@@ -124,18 +60,6 @@ function auth0Logout(cb) {
     localStorage.removeItem('idToken');
     cb()
 }
-
-// function logoutSimple(lock, cb) {
-//     localStorage.removeItem('accessToken');
-
-//     cb();
-
-//     let returnTo = getSiteRootFromUrl(window.location.href);
-
-//     lock.logout({
-//         returnTo
-//     });
-// }
 
 function parseRequestURL() {
 
@@ -153,18 +77,14 @@ function parseRequestURL() {
     return request
 }
 
-// export {
-//     getSiteRootFromUrl,
-//     getUserEmailWithAccessToken,
-//     isAuthenticated,
-//     localLoginSuccess,
-//     renewTokens,
-//     logout,
-//     localLoginSuccessSimple,
-//     isAuthenticatedSimple,
-//     logoutSimple,
-//     parseRequestURL,
-//     getAuth0Lock,
-//     getUserEmail,
-//     getUserProfile
-// }
+
+/*
+      give this: http://localhost:5005/#/family-tree
+      return this: http://localhost:5005/
+      */
+function getSiteRootFromUrl(url) {
+    var parser = document.createElement('a');
+    parser.href = url;
+    let siteRoot = parser.protocol + "//" + parser.host;
+    return siteRoot;
+}
